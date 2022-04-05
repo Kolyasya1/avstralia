433ad42a891b120f6750725ac0b38018ab77048c
Kolyasya1 <88108752+Kolyasya1@users.noreply.github.com>
2022-04-05 11:53:02 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,48 @@
+$(function () {
+	$('.popup__link').magnificPopup({
+		disableOn: 700,
+		type: 'iframe',
+		mainClass: 'mfp-fade',
+		removalDelay: 160,
+		preloader: false,
+
+		fixedContentPos: false
+	});
+
+	$('.gallery__slider').slick({
+		prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="images/gallery/arrowleft.svg" alt=""></button>',
+		nextArrow: '<button type="button" class="slick-btn slick-next"><img src="images/gallery/arrowright.svg" alt=""></button>'
+	});
+
+	$('.gallery__item-inner').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		tLoading: 'Loading image #%curr%...',
+		mainClass: 'mfp-img-mobile',
+		gallery: {
+			enabled: true,
+			navigateByImgClick: true,
+			preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
+		},
+
+	});
+
+
+	$('.menu__btn').on('click', function () {
+		$('.menu__list').toggleClass('menu__list--active');
+	});
+
+
+
+	$('.menu__link').click(function () {
+		var scrollName = $(this).attr('data-scroll'),
+			scrollElem = $(scrollName),
+			scrollTop = scrollElem.offset().top;
+
+		$('html, body').animate({
+			scrollTop: scrollTop 
+		}, 1000);
+	});
+});
+
+
